525145d0911b76ad8fd49bf4640597ceb78b7ede
Shivam <135945623+shivamcsit@users.noreply.github.com>
2024-09-22 21:59:47 +0530

    Add files via upload

---
+++ Code1.cpp
@@ -0,0 +1,19 @@
+//Check for odd and even 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the number to be checked : "<<endl;
+    cin>>n;
+     if(n%2==0)
+     { 
+        cout<<"The entered number is Even "<<endl;
+        }
+        else
+        {
+            cout<<"The entered number is Odd "<<endl;
+        }
+     
+return 0;
+}
